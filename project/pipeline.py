272989b4c22aca33a3e1fc6e60af127f4cd5d067
pratikraut3103 <pratikraut3103@gmail.com>
2024-06-05 15:09:01 +0200

    Projct and exercise

--- project/pipeline.py
+++ project/pipeline.py
@@ -1,26 +1,39 @@
 from kaggle.api.kaggle_api_extended import KaggleApi
 import pandas as pd
 import os
-import shutil
+import zipfile
 import sqlite3
+
+
+def un_zip_file(source):
+    with zipfile.ZipFile(f"{source}.zip", 'r') as zip_ref:
+        zip_ref.extractall(source)
 
 def rename_csv(old,new):
     os.rename(old,new)
-def housing_data(data_path, db_path):
-    api.dataset_download_file('yasserh/housing-prices-dataset', fr'Housing.csv', path=data_path)
-    data = pd.read_csv(fr'{data_path}/Housing.csv')
-    print(data.head())
+
+def vehicle_sales_data(data_path, db_path):
+    api.dataset_download_file('syedanwarafridi/vehicle-sales-data', fr'car_prices.csv', path=data_path)
+    un_zip_file(os.path.join(data_path, 'car_prices.csv'))
+    rename_csv(os.path.join(data_path, 'car_prices.csv'), os.path.join(data_path, 'Vehicle_sales_data.csv'))
+    data = pd.read_csv(fr'{data_path}/Vehicle_sales_data.csv/car_prices.csv')
+    data = data[["year", "state"]]
+    cleaned_data = data.groupby('year').count()
+    cleaned_data.rename(columns={"year": "Year", "state": "Number of Vehicles sold"}, inplace=True)
+    cleaned_data.sort_values(by="year")
     conn = sqlite3.connect(db_path)
-    data.to_sql('Housing_data', conn, index=False, if_exists='replace')
+    cleaned_data.to_sql('Vehicle_sales_data', conn, index=False, if_exists='replace')
 
 
-def economic_data(data_path, db_path):
-    api.dataset_download_file('fernandol/countries-of-the-world', fr'countries of the world.csv',path=data_path)
-    rename_csv(os.path.join(data_path, 'countries%20of%20the%20world.csv'), os.path.join(data_path, 'countries_economic.csv'))
-    data = pd.read_csv(fr'{data_path}/countries_economic.csv')
-    data = data.dropna(axis=0, how='any')
+def co2_emission_data(data_path, db_path):
+    api.dataset_download_file('soheiltehranipour/co2-dataset-in-usa', fr'co2.csv',path=data_path)
+    rename_csv(os.path.join(data_path, 'co2.csv'), os.path.join(data_path, 'co2_emission.csv'))
+    data = pd.read_csv(fr'{data_path}/co2_emission.csv')
+    data["Year"] = data["YYYYMM"].astype(str).str[:4]
+    data.drop(columns="YYYYMM", inplace=True)
+    cleaned_data = data.groupby("Year").sum()
     conn = sqlite3.connect(db_path)
-    data.to_sql('countries_economic', conn, index=False, if_exists='replace')
+    cleaned_data.to_sql('co2_emission', conn, index=False, if_exists='replace')
 
 # Authenticate using kaggle.json
 if __name__ == '__main__':
@@ -34,13 +47,9 @@ if __name__ == '__main__':
     database_name = 'Data.sqlite'
 
     db_path = os.path.join(data_path, database_name)
+    vehicle_sales_data(data_path,db_path)
+    co2_emission_data(data_path,db_path)
 
-    print(data_path)
-    print(current_dir)
-    housing_data(data_path,db_path)
-    print('Housing data done')
-    economic_data(data_path,db_path)
-    print('Economic data done',db_path)
 
 
 
