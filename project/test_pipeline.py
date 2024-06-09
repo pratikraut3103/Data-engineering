22c45346c749b91b50ae2649fdb0ae686baeb4e0
pratikraut3103 <pratikraut3103@gmail.com>
2024-06-09 15:46:35 +0200

    Project 4

--- project/test_pipeline.py
+++ project/test_pipeline.py
@@ -3,7 +3,12 @@ import os
 class TestPipeline(unittest.TestCase):
     def test_check_output_file(self):
         path_current_directory = os.path.dirname(os.path.abspath(__file__))
-        my_sqlite = os.path.join(path_current_directory, 'data', 'data.sqlite')
+
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        base_path = os.path.abspath(os.path.join(current_dir, os.pardir))
+        data_path = os.path.join(base_path, 'data')
+
+        my_sqlite = os.path.join(data_path, 'data.sqlite')
 
         print(my_sqlite)
 
